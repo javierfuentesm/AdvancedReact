0baea5037279d6785465da263ce35e0fbc3a7d53
Javier <javier.fuentesm@hotmail.com>
2020-05-30 15:38:14 -0400

    Styled components

--- .next/server/static/development/pages/_app.js
+++ .next/server/static/development/pages/_app.js
@@ -268,32 +268,45 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _Header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Header */ "./components/Header.js");
 /* harmony import */ var _Meta__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Meta */ "./components/Meta.js");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! styled-components */ "styled-components");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_3__);
 var _jsxFileName = "/Users/rjfm/Documents/Cursos/Advanced-React/sick-fits/frontend/components/Page.js";
 
 
 
+
+var MyButton = styled_components__WEBPACK_IMPORTED_MODULE_3___default.a.button.withConfig({
+  displayName: "Page__MyButton",
+  componentId: "sc-4u7a64-0"
+})(["background:red;font-size:100px;"]);
 
 var Page = function Page(_ref) {
   var children = _ref.children;
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
+      lineNumber: 13
     },
     __self: this
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Meta__WEBPACK_IMPORTED_MODULE_2__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 8
+      lineNumber: 14
     },
     __self: this
   }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Header__WEBPACK_IMPORTED_MODULE_1__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 9
+      lineNumber: 15
     },
     __self: this
-  }), children);
+  }), children, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(MyButton, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "Click Me"));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Page);
@@ -435,6 +448,17 @@ module.exports = require("next/link");
 /***/ (function(module, exports) {
 
 module.exports = require("react");
+
+/***/ }),
+
+/***/ "styled-components":
+/*!************************************!*\
+  !*** external "styled-components" ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("styled-components");
 
 /***/ })
 
--- components/Page.js
+++ components/Page.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from './Header'
 import Meta from './Meta'
+import styled from 'styled-components'
 
+const MyButton = styled.button`
+  background: red;
+  font-size: 100px;
+
+`
 const Page = ({children}) => {
     return (
         <div>
             <Meta/>
             <Header/>
             {children}
+            <MyButton>Click Me</MyButton>
         </div>
     );
 };
